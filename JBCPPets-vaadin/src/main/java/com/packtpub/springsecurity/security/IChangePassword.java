d15213327cf7c2fdbcf2a34a4929b990cdf050f9
Mladen Cikara <mladen.cikara@gmail.com>
2012-07-23 21:37:54 +0200

    Dodao uvijet da password može promjeniti user samo za sebe.

--- JBCPPets-vaadin/src/main/java/com/packtpub/springsecurity/security/IChangePassword.java
+++ JBCPPets-vaadin/src/main/java/com/packtpub/springsecurity/security/IChangePassword.java
@@ -11,13 +11,15 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 public interface IChangePassword extends UserDetailsService {
 
 	/**
-	 * Changes the user's password. Note that a secure implementation would require
-	 * the user to supply their existing password prior to changing it.
+	 * Changes the user's password. Note that a secure implementation would
+	 * require the user to supply their existing password prior to changing it.
 	 * 
-	 * @param username the username
-	 * @param password the new password
+	 * @param username
+	 *            the username
+	 * @param password
+	 *            the new password
 	 */
-	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@PreAuthorize("#username == principal.username and hasRole('ROLE_USER')")
 	void changePassword(String username, String password);
 
 }
